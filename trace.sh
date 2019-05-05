bfbe679bdb557660d46fe4899fb2ead5eb371a28
mintaka33 <mintaka33@outlook.com>
2019-05-05 10:27:44 -0400

    update trace.sh to add more events

--- trace.sh
+++ trace.sh
@@ -1,12 +1,4 @@
 sudo trace-cmd record \
--e "i915:intel_cpu_fifo_underrun" \
--e "i915:intel_pch_fifo_underrun" \
--e "i915:intel_memory_cxsr" \
--e "i915:g4x_wm" \
--e "i915:vlv_wm" \
--e "i915:vlv_fifo_size" \
--e "i915:intel_update_plane" \
--e "i915:intel_disable_plane" \
 -e "i915:i915_pipe_update_start" \
 -e "i915:i915_pipe_update_vblank_evaded" \
 -e "i915:i915_pipe_update_end" \
@@ -25,6 +17,10 @@ sudo trace-cmd record \
 -e "i915:i915_gem_ring_sync_to" \
 -e "i915:i915_request_queue" \
 -e "i915:i915_request_add" \
+-e "i915:i915_request_submit" \
+-e "i915:i915_request_execute" \
+-e "i915:i915_request_in" \
+-e "i915:i915_request_out" \
 -e "i915:intel_engine_notify" \
 -e "i915:i915_request_retire" \
 -e "i915:i915_request_wait_begin" \
